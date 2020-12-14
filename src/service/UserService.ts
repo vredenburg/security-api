3e83e9dd460a1952d49f1c0476ab6904a2275b49
ianvredenburg <ianvredenburg@gmail.com>
2020-12-14 11:15:21 +0100

    Fix incorrect usage of async in new Promise constructor

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -11,137 +11,122 @@ export class UserService {
     }
 
     public find = async (id: string): Promise<User> => {
-        return new Promise(async (resolve, reject) => {
-            let user: User;
+        let user: User;
 
-            try {
-                user = await this.userRepository.findOneOrFail(id);
-            } catch (error) {
-                return reject(error);
-            }
+        try {
+            user = await this.userRepository.findOneOrFail(id);
+        } catch (error) {
+            return Promise.reject(error);
+        }
 
-            return resolve(user);
-        });
+        return Promise.resolve(user);
     };
 
-    public findIdAndPasswordByEmail = (email: string): Promise<User> => {
-        return new Promise(async (resolve, reject) => {
-            let user: User;
-            try {
-                user = await this.userRepository
-                    .createQueryBuilder('user')
-                    .select('user.id')
-                    .addSelect('user.password')
-                    .where('user.email = :email', { email: email })
-                    .getOneOrFail();
-            } catch (error) {
-                return reject(error);
-            }
+    public findIdAndPasswordByEmail = async (email: string): Promise<User> => {
+        let user: User;
 
-            return resolve(user);
-        });
+        try {
+            user = await this.userRepository
+                .createQueryBuilder('user')
+                .select('user.id')
+                .addSelect('user.password')
+                .where('user.email = :email', { email: email })
+                .getOneOrFail();
+        } catch (error) {
+            return Promise.reject(error);
+        }
+
+        return Promise.resolve(user);
     }
 
-    public idExists = (id: string): Promise<boolean> => {
-        return new Promise(async (resolve, reject) => {
-            let count: User | undefined;
+    public idExists = async (id: string): Promise<boolean> => {
+        let count: User | undefined;
 
-            try {
-                count = await this.userRepository
-                    .createQueryBuilder('user')
-                    .where('user.id = :id', { id: id })
-                    .getOne();
-            } catch (error) {
-                return reject(error);
-            }
+        try {
+            count = await this.userRepository
+                .createQueryBuilder('user')
+                .where('user.id = :id', { id: id })
+                .getOne();
+        } catch (error) {
+            return Promise.reject(error);
+        }
 
-            return resolve(count !== undefined);
-        });
+        return Promise.resolve(count !== undefined);
     }
 
-    public emailExists = (email: string): Promise<boolean> => {
-        return new Promise(async (resolve, reject) => {
-            let count: User | undefined;
+    public emailExists = async (email: string): Promise<boolean> => {
+        let count: User | undefined;
 
-            try {
-                count = await this.userRepository
-                    .createQueryBuilder('user')
-                    .where('user.email = :email', { email: email })
-                    .getOne();
-            } catch (error) {
-                return reject(error);
-            }
+        try {
+            count = await this.userRepository
+                .createQueryBuilder('user')
+                .where('user.email = :email', { email: email })
+                .getOne();
+        } catch (error) {
+            return Promise.reject(error);
+        }
 
-            return resolve(count !== undefined);
-        });
+        return Promise.resolve(count !== undefined);
     }
 
     public create = async (newUser: User): Promise<void> => {
-        return new Promise(async (resolve, reject) => {
-
-            try {
-                if (await this.emailExists(newUser.email)) {
-                    return reject(new Error("Email " + newUser.email + " is already in use."));
-                }
-
-                await bcrypt.hash(newUser.password, 10).then(hash => {
-                    newUser.password = hash.toString();
-                    newUser.role = Role.Member;
-                });
-
-                await this.userRepository.save(newUser);
-            } catch (error) {
-                return reject(error);
+        try {
+            if (await this.emailExists(newUser.email)) {
+                return Promise.reject(new Error("Email " + newUser.email + " is already in use."));
             }
 
-            return resolve();
-        });
+            await bcrypt.hash(newUser.password, 10).then(hash => {
+                newUser.password = hash.toString();
+                newUser.role = Role.Member;
+            });
+
+            await this.userRepository.save(newUser);
+        } catch (error) {
+            return Promise.reject(error);
+        }
+
+        return Promise.resolve();
     }
 
     public update = async (updatedUser: User): Promise<void> => {
-        return new Promise(async (resolve, reject) => {
 
-            try {
-                // Check if user exists
-                const userExists: boolean = await this.idExists(updatedUser.id);
+        try {
+            // Check if user exists
+            const userExists: boolean = await this.idExists(updatedUser.id);
 
-                if (!userExists) {
-                    return reject(new Error("User with id " + updatedUser.id + " doesn't exist."));
-                }
-
-                await this.userRepository
-                    .createQueryBuilder()
-                    .update('user')
-                    .set({
-                        firstName: updatedUser.firstName,
-                        lastName: updatedUser.lastName
-                    })
-                    .where('id = :id', { id: updatedUser.id })
-                    .execute();
-            } catch (error) {
-                return reject(error);
+            if (!userExists) {
+                return Promise.reject(new Error("User with id " + updatedUser.id + " doesn't exist."));
             }
 
-            return resolve();
-        });
+            await this.userRepository
+                .createQueryBuilder()
+                .update('user')
+                .set({
+                    firstName: updatedUser.firstName,
+                    lastName: updatedUser.lastName
+                })
+                .where('id = :id', { id: updatedUser.id })
+                .execute();
+        } catch (error) {
+            return Promise.reject(error);
+        }
+
+        return Promise.resolve();
     };
 
     public delete = async (id: string): Promise<void> => {
-        return new Promise(async (resolve, reject) => {
+        try {
+            const userExists: boolean = await this.idExists(id);
 
-            try {
-                const userExists: boolean = await this.idExists(id);
-
-                if (!userExists) {
-                    return reject(new Error("User with id " + id + " doesn't exist."));
-                }
-
-                await this.userRepository.delete(id);
-            } catch (error) {
-                return reject(error);
+            if (!userExists) {
+                return Promise.reject(new Error("User with id " + id + " doesn't exist."));
             }
 
-            return resolve();
-        });
+            await this.userRepository.delete(id);
+        } catch (error) {
+            return Promise.reject(error);
+        }
+
+        return Promise.resolve();
     };
 }
